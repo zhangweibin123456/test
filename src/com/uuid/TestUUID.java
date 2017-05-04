71088d5a4813e8857fe8913db11ef42aeada9318
Administrator <Administrator@20170413-191238>
2017-05-04 17:22:45 +0800

    23

---
+++ src/com/uuid/TestUUID.java
@@ -0,0 +1,17 @@
+package com.uuid;
+
+
+public class TestUUID {
+
+	public static void main(String[] args) {
+//		UUID uuid = UUID.randomUUID().toString().s;
+//		
+//		System.out.println(uuid);
+
+		
+		String str="你好 ${abc} 我";
+		str = str.replace("${abc}", "北京");
+		System.out.println(str);
+	}
+
+}

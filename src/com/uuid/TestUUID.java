d56870c475895ea938ed2ac369072d8c34d7957a
zhangweibin123456 <1215402223@qq.com>
2017-09-29 11:04:43 +0800

    Update TestUUID.java

--- src/com/uuid/TestUUID.java
+++ src/com/uuid/TestUUID.java
@@ -5,12 +5,12 @@ public class TestUUID {
 
 	public static void main(String[] args) {
 
-		System.out.println("21");
-		System.out.println("22");
-		System.out.println("23");
-		System.out.println("24");
-		System.out.println("25");
-		System.out.println("26");
+		System.out.println("31");
+		System.out.println("32");
+		System.out.println("33");
+		System.out.println("34");
+		System.out.println("35");
+		System.out.println("36");
 		
 		
 		

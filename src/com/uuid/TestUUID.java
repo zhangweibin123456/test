5a7bcfeaf642772ce519ed9b06858b004ed83fda
zhangweibin <zhangweibin@DESKTOP-KJI47G7>
2017-09-29 11:00:04 +0800

    12

--- src/com/uuid/TestUUID.java
+++ src/com/uuid/TestUUID.java
@@ -5,12 +5,12 @@ public class TestUUID {
 
 	public static void main(String[] args) {
 
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
-		System.out.println("6");
+		System.out.println("21");
+		System.out.println("22");
+		System.out.println("23");
+		System.out.println("24");
+		System.out.println("25");
+		System.out.println("26");
 		
 		
 		
